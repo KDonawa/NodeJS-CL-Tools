b50b7e090437c2dc3dcdefd2aed012645fbaed36
Kerron Donawa <kerrondonawa@gmail.com>
2021-01-30 11:33:46 -0400

    Added ability to distinguish between files and folders in directory
    using lstat to determine if contents are files or folders
    using chalk to color code folders
    added gitignore for node_modules

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 #!/usr/bin/env node
 
 const fs = require('fs'); // node js file system
+const chalk = require('chalk');
 
-fs.readdir(process.cwd(), (err, files) =>{
+const {lstat} = fs.promises;
+
+fs.readdir(process.cwd(), async (err, files) =>{
     if(err){
         console.log(err);
         return;
     }
-    console.log(files);
+
+    const stats = await Promise.all(files.map(file => lstat(file)));
+    for (let i = 0; i < stats.length; i++) {
+        if(stats[i].isFile()){
+            console.log(files[i]);
+        }
+        else{
+            console.log(chalk.blue(files[i]));
+        }
+    }
 })
